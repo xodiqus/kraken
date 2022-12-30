d25ba0c428ceede0fa9609839a701ed3f8f3b8fb
xodiqus <xodiqus@gmail.com>
2022-12-30 10:08:09 +0600

    Add new functions to KString.

--- parser/kstring.h
+++ parser/kstring.h
@@ -2,21 +2,35 @@
 #ifndef INCLUDED_KSTRING
 #define INCLUDED_KSTRING
 
-#include <string.h>
 #include <stdbool.h>
+#include <stddef.h>
+
+typedef char KRune;
 
 struct KString
 {
-    char*     val;
+    KRune*    val;
     size_t    len;
     bool      owns; 
 };
 
-struct KString span_KString(const char* in, size_t len);
+struct KString span_KString(const KRune* in, size_t len);
 
 // one malloc
-struct KString copy_KString(const char* in, size_t len);
+struct KString make_KString(const KRune* in, size_t len);
+
+// Copies KString, making owner-string.
+struct KString copy_KString(struct KString in);
 
 void free_KString(struct KString);
+
+KRune at_KString(struct KString, size_t i);
+
+struct KString drop_KRunes(struct KString, size_t i);
+struct KString rdrop_KRunes(struct KString, size_t i);
+
+struct KString trim_KString(struct KString); 
+
+bool empty_KString(struct KString);
 
 #endif
--- parser/src/kstring.c
+++ parser/src/kstring.c
@@ -3,28 +3,34 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
-struct KString span_KString(const char* in, size_t len)
+struct KString span_KString(const KRune* in, size_t len)
 {
     struct KString r; 
 
-    r.val = (char*)in;
-    r.len = len != 0 ? strlen(in) : len;
+    r.val = (KRune*)in;
+    r.len = len;
     r.owns = false;
 
     return r;
 } 
 
-struct KString copy_KString(const char* in, size_t len)
+struct KString make_KString(const KRune* in, size_t len)
 {
     struct KString r; 
 
-    r.len = len == 0 ? strlen(in) : len;
-    r.val = malloc(r.len * sizeof(char*));
+    r.len = len;
+    r.val = malloc(r.len * sizeof(KRune));
     r.owns = true;
 
-    memcpy(r.val, in, len);
+    memcpy(r.val, in, r.len * sizeof(KRune));
     return r;
+}
+
+struct KString copy_KString(struct KString in)
+{
+    return make_KString(in.val, in.len);
 }
 
 void free_KString(struct KString in)
@@ -35,3 +41,76 @@ void free_KString(struct KString in)
     }
 }
 
+KRune at_KString(struct KString in, size_t i)
+{
+    assert(i < in.len);
+    return in.val[i];
+}
+
+struct KString drop_KRunes(struct KString in, size_t i)
+{
+    struct KString r; 
+
+    r.val = in.val + i; 
+    r.len = in.len - i;
+    r.owns = false;
+
+    return r;
+}
+
+struct KString rdrop_KRunes(struct KString in, size_t i)
+{
+    struct KString r;
+
+    r.len = in.len - i;
+    r.owns = false;
+    r.val = r.val;
+
+    return r;
+}
+
+struct KString ltrim_KString(struct KString in)
+{
+    if (empty_KString(in)) return span_KString(in.val, 0);
+
+    size_t left = 0;
+    
+    for (size_t i = 0; i < in.len; ++i)
+    {
+        if (at_KString(in, i) == ' ')
+        {
+            ++left;
+        } else break;
+    }
+
+    return drop_KRunes(in, left);
+}
+
+struct KString rtrim_KString(struct KString in)
+{
+    if (empty_KString(in)) return span_KString(in.val, 0);
+
+    size_t right = 0;
+
+    for (size_t i = in.len - 1;; --i)
+    {
+        if (at_KString(in, i) == ' ')
+        {
+            ++right;
+        } else break;
+
+        if (i == 0) break;
+    }
+
+    return rdrop_KRunes(in, right);
+}
+
+struct KString trim_KString(struct KString in)
+{
+    return rtrim_KString(ltrim_KString(in));
+} 
+
+bool empty_KString(struct KString in)
+{
+    return in.len == 0; 
+}
